6d13821544f336b21765fac840d23b2f8e037e7f
Linkeer365 <564142445@qq.com>
2023-01-26 13:59:41 +0800

    2023-01-26_13:59:41

---
+++ merge.py
@@ -0,0 +1,45 @@
+import os
+import time
+cwd1=r"D:\Videos\Captures"
+cwd2=r"D:\autoVideoMerge"
+
+videos=list(map(lambda x:cwd1+os.sep+x, os.listdir(cwd1)))
+videos=sorted(videos,key=os.path.getmtime)[1:]
+
+for idx,i in enumerate(videos,1):
+    print("第{}个：\t{}".format(idx,i))
+print("\n\n")
+ask_start=input("从第几个视频开始（包括该视频）：")
+ask_end=input("到第几个视频结束（包括该视频）：")
+
+videos=videos[int(ask_start)-1:int(ask_end)]
+
+for idx,i in enumerate(videos,1):
+    print("第{}个：\t{}".format(idx,i))
+print("\n\n")
+
+os.chdir(cwd1)
+name_idx,name=0,None
+for vdo in videos:
+    vdo_file=vdo
+    # print(vdo)
+    with open("mylist.txt","a",encoding="gbk") as f:
+        f.write(f"file \'{vdo_file}\'\n")
+    if name_idx==0:
+        name=vdo.replace(".mp4", "_all.mp4")
+        name_idx+=1
+
+finish_file=name
+
+start=time.time()
+
+concat_comm=f"ffmpeg -f concat -safe 0 -i mylist.txt -c copy \"{finish_file}\" -y"
+os.system(concat_comm)
+
+end=time.time()
+
+print("one done.")
+print("Time Used:{}".format(end-start))
+os.remove("mylist.txt")
+
+# print(videos)
